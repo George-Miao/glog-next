2bd3561e113be08b21f8ca36b7b57ec43ba03a93
George Miao <gm@miao.dev>
2022-01-24 07:42:56 -0500

    feat: major layout of the entire site

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,48 @@
+import { defineVFCWithChild } from '@core/utils'
+import NavBar from '../navbar'
+import Footer from '../footer'
+
+/**
+ * When using layout, it is recommended to use
+ * SafeArea inside for constrain the max width to 3xl
+ *
+ * Here I use 6xl for background for the sake of aesthetics
+ * however content should
+ * not take the entire <main />
+ */
+const Layout = defineVFCWithChild(({ children, className }) => {
+  return (
+    <div
+      className="min-h-screen
+      bg-red-800 relative
+      ">
+      <NavBar />
+
+      {/* Main container */}
+      <main
+        className={`
+          relative
+          pb-8 pt-[max(8vh,6rem)]
+          sm:(p-8 pt-[max(8vh,6rem)])
+          lg:(pt-8 ml-56)
+        `}>
+        {/* White Background, max-w-6xl */}
+        <div
+          className={`${className ?? ''}
+            shadow-main min-h-[calc(100vh-12rem)]
+            relative z-100 bg-light-100
+            max-w-6xl mx-auto
+            px-6 py-8
+            sm:(px-16 py-20)
+            md:(px-26 py-32)
+            lg:min-h-[calc(100vh-8rem)]`}>
+          {children}
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  )
+})
+
+export default Layout
