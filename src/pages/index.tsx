995f102f0c27ec127e3ecc76f04487983922fe41
George Miao <gm@miao.dev>
2022-01-30 19:48:37 -0500

    content: Tharsis introduction at index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import content from '@styles/content.module.css'
 
 import type { DotListItemProp } from '@comps/dotList'
 import type { GetStaticProps } from 'next'
+import Link from 'next/link'
 
 const resumeList: DotListItemProp[] = [
   {
@@ -41,10 +42,16 @@ const resumeList: DotListItemProp[] = [
   { title: 'Montverde Academy', value: '2018 - 2020', subtitle: 'Highschool' }
 ]
 
-const image = {
+const mapImage = {
   src: 'https://imagedelivery.net/b21oeeg7p6hqWEI-IA5xDw/e0f525ed-8419-4e08-1ebf-b7a1eb208300/public',
   placeholder:
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAFCAIAAADzBuo/AAAACXBIWXMAAAsTAAALEwEAmpwYAAAApklEQVQImQGbAGT/AABFlAB9i5e+YpDFboW9cXezcmyqcm2fYQBvhAAkYQAASpgfmI2z1XOy3XyVx2Z8uH14rWtelmoAQX0ADFEAAD+Rca6J1ueRp9B5pdBpjr5vkrtlAGpyACFwAA5SAAA/kQB8neDsn8Llia3aeJfNXYnBZABOhAAbZgALUAAARZQAWKGr3bb//6qv25E3m5c6oaIATIwAF2AACE7N5kOMxiFKnAAAAABJRU5ErkJggg=='
+}
+
+const tharsisImage = {
+  src: 'https://imagedelivery.net/b21oeeg7p6hqWEI-IA5xDw/9a60b7f0-9440-48dd-432d-7d6c1d498900/public',
+  placeholder:
+    'data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAKAAgDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAAQG/8QAIhAAAQMDAwUAAAAAAAAAAAAAAgEDEQAEBQYHIRMiUnGB/8QAFAEBAAAAAAAAAAAAAAAAAAAABP/EABkRAQACAwAAAAAAAAAAAAAAAAEAAhJBcf/aAAwDAQACEQMRAD8AzGx+rMyy+1avZi7ZxqXAqTaLASpyRKseKRH2lTaLbBt8EABFOqR8JHdzz7pRsg1EXFu9n//Z'
 }
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -53,6 +60,11 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {}
   }
 }
+
+const emClass = `w-full inline-block
+my-2 px-4
+flex flex-row items-center
+text-warm-gray-600`
 
 const Home = definePage(() => {
   return (
@@ -68,22 +80,17 @@ const Home = definePage(() => {
         <br />
         <h3 className="my-6">Wait, where is it?</h3>
         <Image
-          className="transition-all "
-          alt="Map of location"
+          className="transition-all"
+          alt="Map of my location"
           layout="intrinsic"
-          src={image.src}
-          blurDataURL={image.placeholder}
+          src={mapImage.src}
+          blurDataURL={mapImage.placeholder}
           placeholder="blur"
           width={1112}
           height={590}
           about="Syracuse, NY"
         />
-        <em
-          className="w-full inline-block
-            my-2 px-4
-            flex flex-row items-center
-            text-warm-gray-600"
-        >
+        <em className={emClass}>
           <div className="flex-grow" />
           <Icon
             icon="akar-icons:location"
@@ -93,6 +100,31 @@ const Home = definePage(() => {
           />
           <span className="text-xs md:text-sm">Syracuse, NY</span>
         </em>
+        <br />
+        <p>
+          And, let me proudly introduce you, my home server rack,{' '}
+          <b>Tharsis:</b>
+        </p>
+
+        <Image
+          className="transision-all"
+          alt="Photo of Tharsis"
+          src={tharsisImage.src}
+          placeholder="blur"
+          blurDataURL={tharsisImage.placeholder}
+          width={3024}
+          height={4032}
+        />
+
+        <p className="mt-8">
+          It&apos;s at China so currently I&apos;m not able to access it,
+          physically. But with Cloudflare Tunnel, I can still access{' '}
+          <Link href="/projects">
+            <a>services runs on it</a>
+          </Link>{' '}
+          and use ssh. So I&apos;m happy with it. After all I don&apos;t have a
+          public IP address and basically everything in China is behind NAT.
+        </p>
       </div>
 
       <DotList items={resumeList} title="Resume" className="mb-16" />
