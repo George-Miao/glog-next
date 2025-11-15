c4d55c14e34777d09e9d8f840eb3e4ac11ba8d53
George Miao <gm@miao.dev>
2025-11-14 23:50:34 -0500

    feat: prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,23 @@
 import type { Config } from '@react-router/dev/config'
+import { getPostList } from 'app/build/post/reduce'
 
 export default {
-  ssr: true
+  ssr: true,
+
+  async prerender() {
+    const fixed = [
+      '/',
+      '/writing',
+      '/changelog',
+      '/projects',
+      '/thingy',
+      '/writing/tags',
+      '/writing/categories'
+    ]
+    const posts = await getPostList().then(x =>
+      x.map(post => `/writing/${post.slug}`)
+    )
+
+    return fixed.concat(posts)
+  }
 } satisfies Config
